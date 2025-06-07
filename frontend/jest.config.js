35d8931f63368a4286c2868aec5b402853f5ba7f
Ananya-practice <hipperfluffs@gmail.com>
2025-06-07 17:49:24 +0530

    check

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,15 +1,13 @@
 module.exports = {
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+    '^.+\\.[jt]sx?$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    "node_modules/(?!(axios)/)"
+    // Ignore node_modules except axios
+    'node_modules/(?!(axios)/)',
   ],
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
   moduleNameMapper: {
-    '^axios$': 'axios/dist/node/axios.cjs.js',
+    '^axios$': 'axios/dist/node/axios.cjs.js', // <-- force CJS axios
   },
-
-
-
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
 };
