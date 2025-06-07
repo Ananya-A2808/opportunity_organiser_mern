9c423e729fe2d7a2e979ddb798b8e0b02b9ee77d
Ananya-practice <hipperfluffs@gmail.com>
2025-06-07 18:13:04 +0530

    check

--- frontend/src/__tests__/ResumeBuilder_new.test.js
+++ frontend/src/__tests__/ResumeBuilder_new.test.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ResumeBuilderNew from '../pages/ResumeBuilder_new';
+import axios from 'axios';
+
+jest.mock('axios', () => ({
+  get: jest.fn(),
+  post: jest.fn(),
+}));
 
 describe('ResumeBuilder_new Component', () => {
   test('renders ResumeBuilder_new component without crashing', () => {
