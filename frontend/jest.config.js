a36fa4af0c2427c7694d9400909f8d39ec35faa2
Ananya-practice <hipperfluffs@gmail.com>
2025-06-07 17:17:31 +0530

    check

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
   transformIgnorePatterns: [
-    "node_modules/(?!(axios)/)"
+    "node_modules/(?!(axios)/)", // transform axios, but ignore other node_modules
   ],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"], // if you have setupTests.js
 };
